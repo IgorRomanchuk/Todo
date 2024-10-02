c4ca634d89356dc52bd3188d7b2de05f632b773d
Igor Romanchuk <defhn900@gmail.com>
2024-10-02 15:24:40 +0300

    AuthResponse moved to dto folder

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -2,24 +2,33 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/api";
 import { AuthModel } from "../models/auth.model";
-
-export type AuthResponse = {
-  access_token: string;
-};
+import { AuthResponse } from "../utils/dto/authorization";
 
 class AuthService {
   endpoint: string = "auth";
 
   signUp = async (params: AuthModel) =>
-    (await axios.post<AuthResponse>(`${BASE_URL}${this.endpoint}/register`, params)).data;
+    (
+      await axios.post<AuthResponse>(
+        `${BASE_URL}${this.endpoint}/register`,
+        params
+      )
+    ).data;
 
   signIn = async (params: AuthModel) =>
-    (await axios.post<AuthResponse>(`${BASE_URL}${this.endpoint}/login`, params)).data;
+    (
+      await axios.post<AuthResponse>(
+        `${BASE_URL}${this.endpoint}/login`,
+        params
+      )
+    ).data;
 
   profile = async (token: string | null) =>
-    (await axios.get(`${BASE_URL}${this.endpoint}/profile`, {
-      headers: { Authorization: `Bearer ${token}` },
-    })).data;
+    (
+      await axios.get(`${BASE_URL}${this.endpoint}/profile`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+    ).data;
 
   getToken = () => localStorage.getItem("todo-token");
 
