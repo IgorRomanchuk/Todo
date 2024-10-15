4bccd9975606123f9977695c05a8db75db02a17b
Igor Romanchuk <defhn900@gmail.com>
2024-10-15 10:06:54 +0300

    arrow for tooltip

--- src/components/AppointmentsTable/TableDataCell/Tooltip/styles.ts
+++ src/components/AppointmentsTable/TableDataCell/Tooltip/styles.ts
@@ -7,10 +7,33 @@ export const TooltipStyle = styled.div`
   border-radius: 5px;
   padding: 5px 0;
   position: absolute;
-  z-index: 50;
-  bottom: 105%;
+  z-index: 1000;
+  bottom: 115%; 
   left: 50%;
   transform: translateX(-50%);
   opacity: 0;
   transition: opacity 0.3s;
+  border: 1px solid black;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 100%; 
+    left: 50%;
+    transform: translateX(-50%);
+    border-width: 5px;
+    border-style: solid;
+    border-color: #f0f0f0 transparent transparent transparent;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 100%; 
+    left: 50%;
+    transform: translateX(-50%);
+    border-width: 6px;
+    border-style: solid;
+    border-color: black transparent transparent transparent;
+  }
 `;
